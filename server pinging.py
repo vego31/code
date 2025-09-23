5e95d8b7636b6614bf17e52d18eeae2932180a78
vego31 <vegard3110@hotmail.com>
2025-09-23 11:40:59 +0200

    idk what im doing

--- server pinging.py
+++ server pinging.py
@@ -1,7 +1,11 @@
 import json
+from pythonping import ping
 
-print("hello world")
 
+print("Welcome to server pinger")
+print("1. Add server")
+print("2. show servers status")
+option = input("Choose an option:")
 
 def addserver():
     print("add server")
@@ -11,3 +15,28 @@ def addserver():
     ip = input("server ip:")
     print("server ip is %s" % ip)
     
+    jsondata = {}
+    jsondata["id"] = server
+    jsondata["server"] = server
+    jsondata["ip"] = ip
+    
+    with open("servers.json", "a") as f:
+        json.dump(jsondata, f)
+        f.write("\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if option == "1":
+    addserver()
+
