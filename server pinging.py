c570b1d92fb78be5da825e6bc634e476e55b8618
vego31 <vegard3110@hotmail.com>
2025-09-23 10:54:37 +0200

    first commit

---
+++ server pinging.py
@@ -0,0 +1,13 @@
+import json
+
+print("hello world")
+
+
+def addserver():
+    print("add server")
+    
+    server = input("server name:")
+    print("server name is %s" % server)
+    ip = input("server ip:")
+    print("server ip is %s" % ip)
+    
